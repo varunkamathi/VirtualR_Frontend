43faf2ec1edf05888ccab92b0909fd88eb4bc43c
varunkamathi <varunkamathi522004@gmail.com>
2025-04-02 18:50:54 +0530

    create feature section

--- react_client/src/App.jsx
+++ react_client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Nav from './components/Nav'
 import HeroSection from './components/HeroSection'
+import Feature from './components/Feature'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <div className='max-w-7xl mx-auto pt-20 px-6'>
     <HeroSection/>
     </div>
+    <Feature/>
     </>
   )
 }
--- react_client/src/components/Nav.jsx
+++ react_client/src/components/Nav.jsx
@@ -51,7 +51,7 @@ const Nav = () => {
                     <a href="#" className='py-2 px-3 border rounded-md'>
                         Sign In
                     </a>
-                    <a href="#" className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md'>
+                    <a href="#"  className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md'>
                         Create an account
                     </a>
                 </div>
